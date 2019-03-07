c8f6c605c542285962eaf4d6ff1be3a28902b464
Maciej Augustyniak <maciej.f.august@gmail.com>
2019-03-07 02:30:47 +0100

    Hide cursor

--- src/main.rs
+++ src/main.rs
@@ -207,6 +207,7 @@ impl Matrix {
 
 fn main() {
     let root = nc::initscr();
+    nc::curs_set(nc::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     nc::start_color();
     nc::init_pair(GREEN, nc::COLOR_GREEN, nc::COLOR_BLACK);
     nc::init_pair(LIGHT_GREEN, nc::COLOR_WHITE, nc::COLOR_BLACK);
